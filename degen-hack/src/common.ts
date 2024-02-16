b1b217270ca125d3d283c76efbeb084ef8e2c343
Orest Tarasiuk <830847+OrestTa@users.noreply.github.com>
2024-02-16 21:22:55 +0100

    add send tx demo, but still failing rn

--- degen-hack/src/common.ts
+++ degen-hack/src/common.ts
@@ -1,3 +1,40 @@
-export default function sendTestTx() {
-  console.log("sendTestTx");
+import { FuseSDK } from "@fuseio/fusebox-web-sdk";
+import { ethers } from "ethers";
+import { generatePrivateKey } from "viem/accounts";
+import { API_KEY } from "./env";
+
+export default async function sendTestTx() {
+  console.log("sendTestTx...");
+
+  const privateKey =
+    process.env.PRIVATE_KEY ??
+    (() => {
+      const pk = generatePrivateKey();
+      console.log(pk);
+      return pk;
+    })();
+
+  const apiKey = API_KEY;
+  const credentials = new ethers.Wallet(privateKey);
+  const fuseSDK = await FuseSDK.init(apiKey, credentials, {
+    withPaymaster: true,
+  });
+
+  console.log(
+    `Smart account address: https://explorer.fuse.io/address/${fuseSDK.wallet.getSender()}`
+  );
+
+  const to = "0xd8da6bf26964af9d7eed9e03e53415d37aa96045"; // for vitalik.eth
+  const value = ethers.utils.parseEther("0");
+  const data = new TextEncoder().encode("0x1234");
+  const res = await fuseSDK.callContract(to, value, data);
+
+  console.log(`UserOpHash: ${res?.userOpHash}`);
+  console.log("Waiting for transaction...");
+
+  const receipt = await res?.wait();
+
+  console.log(
+    `User operation included: https://explorer.fuse.io/tx/${receipt?.transactionHash}`
+  );
 }
